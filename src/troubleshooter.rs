58366dd449f380afa2ceb6082fd3eaa69eee152e
Yifei Sun <ysun@hey.com>
2026-08-05 21:46:44 +0200

    treewide: drop support for Nix older than 2.10

--- src/troubleshooter.rs
+++ src/troubleshooter.rs
@@ -7,10 +7,10 @@ use std::future::Future;
 
 use snafu::ErrorCompat;
 
-use crate::{error::ColmenaError, nix::HivePath};
+use crate::error::ColmenaError;
 
 /// Runs a closure and tries to troubleshoot if it returns an error.
-pub async fn run_wrapped<F, T>(f: F, hive_config: Option<HivePath>) -> T
+pub async fn run_wrapped<F, T>(f: F) -> T
 where
     F: Future<Output = Result<T, ColmenaError>>,
 {
@@ -20,12 +20,7 @@ where
             tracing::error!("-----");
             tracing::error!("Operation failed with error: {}", error);
 
-            if let Err(own_error) = troubleshoot(hive_config, &error) {
-                tracing::error!(
-                    "Error occurred while trying to troubleshoot another error: {}",
-                    own_error
-                );
-            }
+            troubleshoot(&error);
 
             // Ensure we exit with a code
             quit::with_code(1);
@@ -33,25 +28,7 @@ where
     }
 }
 
-fn troubleshoot(hive_config: Option<HivePath>, error: &ColmenaError) -> Result<(), ColmenaError> {
-    if let ColmenaError::NoFlakesSupport = error {
-        // People following the tutorial might put hive.nix directly
-        // in their Colmena checkout, and encounter NoFlakesSupport
-        // because Colmena always prefers flake.nix when it exists.
-
-        if hive_config.is_none() {
-            let cwd = env::current_dir()?;
-            if cwd.join("flake.nix").is_file() && cwd.join("hive.nix").is_file() {
-                eprintln!(
-                    "Hint: You have both flake.nix and hive.nix in the current directory, and"
-                );
-                eprintln!("      Colmena will always prefer flake.nix if it exists.");
-                eprintln!();
-                eprintln!("      Try passing `-f hive.nix` explicitly if this is what you want.");
-            }
-        };
-    }
-
+fn troubleshoot(error: &ColmenaError) {
     if let Some(bt) = ErrorCompat::backtrace(error) {
         if backtrace_enabled() {
             eprintln!("Backtrace:");
@@ -62,8 +39,6 @@ fn troubleshoot(hive_config: Option<HivePath>, error: &ColmenaError) -> Result<(
             );
         }
     }
-
-    Ok(())
 }
 
 fn backtrace_enabled() -> bool {
